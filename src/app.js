46e01bc485c9713cdcd12b7054e62d39ddfa643e
KanhaiyaLalTeli <92908760+KanhaiyaLalTeli@users.noreply.github.com>
2024-10-02 20:52:24 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const PORT=process.env.port || 8080;
 
 
 app.use(cors({
-    origin: "http://localhost:5173/",
+    origin: "https://devfinder-fe.onrender.com",
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
 }
